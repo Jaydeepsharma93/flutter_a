fa320c2ee26c102debcc7f7cc91bf933f71ff8a1
jaydeepsharma93 <sharmajaydeep1515@gmail.com>
2024-02-26 14:55:04 +0530

    21/02/2024

--- lib/Drawer & Buttons/drawer.dart
+++ lib/Drawer & Buttons/drawer.dart
@@ -1,22 +1,33 @@
 import 'package:flutter/material.dart';
 
 void main(){
-  runApp(const Drawer());
+  runApp(const D_rawer());
 }
-class Drawer extends StatefulWidget {
-  const Drawer({super.key});
+class D_rawer extends StatefulWidget {
+  const D_rawer({super.key});
 
   @override
-  State<Drawer> createState() => _DrawerState();
+  State<D_rawer> createState() => _D_rawerState();
 }
 
-class _DrawerState extends State<Drawer> {
+class _D_rawerState extends State<D_rawer> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: SafeArea(
         child: Scaffold(
+          backgroundColor: Color(0xff2e2f33),
+          appBar: AppBar(
+            iconTheme: IconThemeData(color: Colors.white),
+            backgroundColor: Colors.black,
+            title: Text('Gmail',style: TextStyle(color: Colors.white),),
+          ),
+          drawer: Container(
+            height:double.infinity,
+            width: 300,
+            color: Color(0xff2e2f33)
+          )
         ),
       ),
     );
